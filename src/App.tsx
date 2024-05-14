c8421adae280906d63a8d4cd061c0b0a9246ce77
Brayl Amaquin <brayljamesamaquin@gmail.com>
2024-05-14 08:17:16 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import {Button, ButtonGroup} from "@nextui-org/button";
+
+function App() {
+
+  return (
+    <Button color="primary">
+      Button
+    </Button>
+  );
+}
+
+export default App
